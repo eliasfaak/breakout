a7a2cb1a822a0e18d3dea8083d22e9ddf81d4301
elias <eliasfaak@gmail.com>
2021-02-16 12:38:33 +0100

    faster machine

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+from agent import Agent
 os.environ["KERAS_BACKEND"]="plaidml.keras.backend"
 from keras.layers import Dense, Activation, Conv2D, MaxPooling2D, Flatten
 from keras.models import Sequential
@@ -6,8 +7,31 @@ from tensorflow.keras.models import load_model
 from keras.optimizers import Adam
 import gym
 import numpy as np
+if __name__ == "__main__":
+    env = gym.make("Breakout-ram-v0")
+    player = Agent(gamma=0.99, epsilon=1.0, alpha=0.0005, input_dims=9,
+                    n_actions = 4, mem_size=1000000, batch_size=64, epsilon_end=0.01)
+    player.load_model()
+    def train_n_games(n):
+        game_counter = 0
+        for i in range(n):
+            obs = env.reset()
+            done = False
+            while not done:
+                action = player.choose_action(obs)
+                obs, reward, done, info = env.step(action)
+                player.learn()
+            game_counter+=1
+            player.save_model()
+            print("games played = %i with reward %f" % (game_counter, reward))
 
-env = gym.make("Breakout-ram-v0")
-env.reset()
-obs, rew, done, info = env.step(env.action_space.sample())
-
+    def play_n_games(n):
+        for i in range(n):
+            obs = env.reset()
+            done = False
+            while not done:
+                env.render()
+                action = player.choose_action(obs)
+                obs, reward, done, info = env.step(action)
+    #train_n_games(100)
+    play_n_games(2)
